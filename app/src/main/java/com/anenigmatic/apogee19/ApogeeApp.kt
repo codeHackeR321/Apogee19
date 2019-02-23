bd658a3ff98f1de33ae8557a262753befb0bd5ad
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-02-23 16:05:14 +0530

    Make initial commit
    Remove "Hello World" TextView and example tests. Change XML code style.
    Make Application subclass and refactor MainActivity into
    screens.shared.view package. Set app's base theme to NoActionBar. Add
    Mockito.

---
+++ app/src/main/java/com/anenigmatic/apogee19/ApogeeApp.kt
@@ -0,0 +1,5 @@
+package com.anenigmatic.apogee19
+
+import android.app.Application
+
+class ApogeeApp : Application()
---
+++ app/src/main/java/com/anenigmatic/apogee19/screens/shared/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.anenigmatic.apogee19.screens.shared.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anenigmatic.apogee19.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_main)
+    }
+}
