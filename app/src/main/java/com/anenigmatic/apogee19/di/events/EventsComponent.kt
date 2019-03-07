15ae0acb5a3604633ff135624e0603aa9efff7f0
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-03-07 15:09:12 +0530

    Make ViewModelFactory for EventListFragment and setup di

--- app/src/main/java/com/anenigmatic/apogee19/di/events/EventsComponent.kt
+++ app/src/main/java/com/anenigmatic/apogee19/di/events/EventsComponent.kt
@@ -1,6 +1,10 @@
 package com.anenigmatic.apogee19.di.events
 
+import com.anenigmatic.apogee19.screens.events.core.EventListViewModelFactory
 import dagger.Subcomponent
 
 @Subcomponent(modules = [EventsModule::class])
-interface EventsComponent
+interface EventsComponent {
+
+    fun inject(factory: EventListViewModelFactory)
+}
