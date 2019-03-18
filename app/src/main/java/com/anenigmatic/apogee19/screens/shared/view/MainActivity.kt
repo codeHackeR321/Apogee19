eab7a1a1ba7c343f5dbb08d93bace6136529f338
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-03-18 13:12:53 +0530

    Perform intial setup of the bottom navigation bar
    Since no screen other than the events screen has been made till now,
    tapping on a bottom navigation tab simply displays a toast.

--- app/src/main/java/com/anenigmatic/apogee19/screens/shared/view/MainActivity.kt
+++ app/src/main/java/com/anenigmatic/apogee19/screens/shared/view/MainActivity.kt
@@ -2,8 +2,14 @@ package com.anenigmatic.apogee19.screens.shared.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentManager
 import com.anenigmatic.apogee19.R
 import com.anenigmatic.apogee19.screens.events.view.EventListFragment
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationAdapter
+import kotlinx.android.synthetic.main.act_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +21,50 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .add(R.id.rootPOV, EventListFragment())
                 .commitNow()
+        }
+
+        setupBottomNavigation()
+    }
+
+
+    private fun setupBottomNavigation() {
+        val navigationAdapter = AHBottomNavigationAdapter(this, R.menu.mn_bottom_nav)
+        navigationAdapter.setupWithBottomNavigation(bottomNavAHB)
+
+        bottomNavAHB.defaultBackgroundColor = ContextCompat.getColor(this, R.color.vio08)
+
+        bottomNavAHB.accentColor = ContextCompat.getColor(this, R.color.vio07)
+        bottomNavAHB.inactiveColor = ContextCompat.getColor(this, R.color.pnk01)
+
+        bottomNavAHB.titleState = AHBottomNavigation.TitleState.ALWAYS_HIDE
+
+        bottomNavAHB.setCurrentItem(3, false)
+
+        bottomNavAHB.setOnTabSelectedListener { position, wasSelected ->
+
+            // This removes all the Fragments except the EventListFragment(which is the root)
+            supportFragmentManager.popBackStackImmediate(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+
+            if(!wasSelected) {
+                when(position) {
+                    0 -> {
+                        Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
+                    }
+                    1 -> {
+                        Toast.makeText(this, "Menu", Toast.LENGTH_SHORT).show()
+                    }
+                    2 -> {
+                        Toast.makeText(this, "Orders", Toast.LENGTH_SHORT).show()
+                    }
+                    3 -> {
+                        // Since events screen is the root, we don't need to replace it here.
+                    }
+                    4 -> {
+                        Toast.makeText(this, "More", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            true
         }
     }
 }
