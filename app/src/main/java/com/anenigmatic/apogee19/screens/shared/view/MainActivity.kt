0b1e35815546086a7664f92a06a0ed99203f0e89
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-03-18 11:36:18 +0530

    Make the MainActivity show events screen on startup

--- app/src/main/java/com/anenigmatic/apogee19/screens/shared/view/MainActivity.kt
+++ app/src/main/java/com/anenigmatic/apogee19/screens/shared/view/MainActivity.kt
@@ -3,11 +3,18 @@ package com.anenigmatic.apogee19.screens.shared.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.anenigmatic.apogee19.R
+import com.anenigmatic.apogee19.screens.events.view.EventListFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_main)
+
+        if(savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.rootPOV, EventListFragment())
+                .commitNow()
+        }
     }
 }
